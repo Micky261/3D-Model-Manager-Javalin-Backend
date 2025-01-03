058998c71d5b9621b801e09dfd0159dcb21a11ba
Frank Nelles <f.nelles@hotmail.com>
2025-01-03 13:08:15 +0100

    Fix Printables import

--- src/main/kotlin/utils/Extensions.kt
+++ src/main/kotlin/utils/Extensions.kt
@@ -1,8 +1,11 @@
 package utils
 
 import com.github.kittinunf.fuel.core.Request
+import data.importer.BaseImporter
 import okhttp3.HttpUrl.Companion.toHttpUrl
 
 fun String.toUrl() = this.toHttpUrl().toString()
 
 fun Request.authToken(token: String) = this.header(mapOf("Authorization" to "Token $token"))
+
+fun Request.ua() = this.header("User-Agent", BaseImporter.USER_AGENT)
